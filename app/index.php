7c08f8f4237abbe4c302d092bd8d15212c2f3df3
Guillaume Belleuvre <guillaumebelleuvre@gmail.com>
2024-05-21 13:53:03 +0200

    first commit

--- app/index.php
+++ app/index.php
@@ -31,10 +31,16 @@
             <p class="exercice-txt">Ecrivez la phrase suivante dans une balise HTML P en utilisant les 2 variables ci-dessous :</p>
             <p class="exercice-txt">"<i>Firstname</i> a obtenu <i>score</i> points à cette partie."</p>
             <div class="exercice-sandbox">
-            <?php
+
+                <?php
                 $firstname = "Michel";
                 $score = 327;
-            ?>
+
+                echo "<p> {$firstname} a obtenu {$score}  points à cette partie. </p>";
+                
+                echo '<p>' . $firstname . ' a obtenu ' . $score . ' points à cette partie.  </p>';
+                ?>
+                <p> <?= $firstname ?> a obtenu <?= $score ?> points à cette partie.</p>
             </div>
         </section>
 
@@ -44,14 +50,14 @@
             <h2 class="exercice-ttl">Question 2</h2>
             <p class="exercice-txt">Afficher dans une liste HTML le nom des produits suivants et leurs prix.</p>
             <div class="exercice-sandbox">
-            <?php
+                <?php
                 $nameProduct1 = "arc";
                 $priceProduct1 = 10.30;
                 $nameProduct2 = "flèche";
                 $priceProduct2 = 2.90;
                 $nameProduct3 = "potion";
                 $priceProduct3 = 5.20;
-            ?>
+                ?>
             </div>
         </section>
 
@@ -60,11 +66,11 @@
             <h2 class="exercice-ttl">Question 3</h2>
             <p class="exercice-txt">Calculer le montant total de la commande des produits ci-dessus avec les quantités ci-dessous et appliquez lui une remise de 10%.</p>
             <div class="exercice-sandbox">
-            <?php
+                <?php
                 $quantityProduct1 = 1;
                 $quantityProduct2 = 10;
                 $quantityProduct3 = 4;
-            ?>
+                ?>
             </div>
         </section>
 
@@ -74,7 +80,7 @@
             <h2 class="exercice-ttl">Question 4</h2>
             <p class="exercice-txt">Affichez le prix le plus élevé des 3 produits ci-dessus.</p>
             <div class="exercice-sandbox">
-                
+
             </div>
         </section>
 
@@ -88,7 +94,7 @@
             <h2 class="exercice-ttl">Question 5</h2>
             <p class="exercice-txt">Affichez dans une liste HTML le nom des produits de la question 2 qui sont présents dans la phrase : "<?= $text1 ?>"</p>
             <div class="exercice-sandbox">
-                
+
             </div>
         </section>
 
@@ -97,7 +103,7 @@
             <h2 class="exercice-ttl">Question 6</h2>
             <p class="exercice-txt">Parmis les scores suivants, affichez le prénom des joueurs qui ont obtenus entre 50 et 150 points.</p>
             <div class="exercice-sandbox">
-            <?php
+                <?php
                 $namePlayer1 = "Tim";
                 $scorePlayer1 = 67;
                 $namePlayer2 = "Morgan";
@@ -108,7 +114,7 @@
                 $scorePlayer4 = 134;
                 $namePlayer5 = "Kevin";
                 $scorePlayer5 = 103;
-            ?>
+                ?>
             </div>
         </section>
 
@@ -118,7 +124,7 @@
             <h2 class="exercice-ttl">Question 7</h2>
             <p class="exercice-txt">En réutilisant les scores de la question précédente, afficher le nom du joueur ayant obtenu le plus grand score.</p>
             <div class="exercice-sandbox">
-                
+
             </div>
         </section>
 
@@ -128,7 +134,7 @@
             <h2 class="exercice-ttl">Question 8</h2>
             <p class="exercice-txt">Affichez le prénom du joueur le plus long en nombre de caractères.</p>
             <div class="exercice-sandbox">
-                
+
             </div>
         </section>
 
@@ -145,7 +151,7 @@
             </ul>
             <p class="exercice-txt">Afficher la valeur de cette variable avec tous les détails.</p>
             <div class="exercice-sandbox">
-                
+
             </div>
         </section>
 
@@ -154,7 +160,7 @@
             <h2 class="exercice-ttl">Question 10</h2>
             <p class="exercice-txt">Afficher le prénom et l'âge du joueur le plus jeune dans une phrase dans une balise HTML P.</p>
             <div class="exercice-sandbox">
-                
+
             </div>
         </section>
     </div>
